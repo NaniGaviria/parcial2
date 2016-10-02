607d731f2bb08ccf83f6969c36cf8527d6ff92c7
Roberto Robles <robertodavidrobles@gmail.com>
2016-10-01 22:43:44 -0500

    agrega metodos de get/set

--- Lan.java
+++ Lan.java
@@ -4,5 +4,22 @@ public class Lan {
   protected String capacidad;
   protected int numNodos;
 
+public Lan(String tipoDeComunicacion,String capacidad){
+  this.tipoDeComunicacion = tipoDeComunicacion;
+  this.capacidad = capacidad;
+}
+
+protected String getTipoDeComunicacion(){
+  return tipoDeComunicacion;
+}
+protected String getCapacidad(){
+  return capacidad;
+}
+protected void setTipoDeComunicacion(){
+  this.tipoDeComunicacion = tipoDeComunicacion;
+}
+protected void setCapocidad(){
+  this.capacidad = capacidad;
+}
 
 }
