1a8a4352130b628aa95a5d395b42950e09dcef25
Lina Oll <ferchalina88@hotmail.com>
2016-10-02 16:24:22 -0500

    el constructor de Anillo

---
+++ Anillo.java
@@ -0,0 +1,8 @@
+public class Anillo() extends Lan{
+
+protected String pass = "";
+
+public Anillo(String tipoDeComunicacion,String capacidad,String pass){
+  super(tipoDeComunicacion,capacidad);
+  this.pass = pas;
+}
