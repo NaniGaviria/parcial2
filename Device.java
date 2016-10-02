b971c4ebdb8164eb355ad01db37472e4ee8aee96
Roberto Robles <robertodavidrobles@gmail.com>
2016-10-02 15:42:48 -0500

    add atribute id and metods get/set

--- Device.java
+++ Device.java
@@ -2,21 +2,26 @@ class Device{
 
 String tipo = "";
 String dato = "";
+String id = "";
 
-public Device(String tipo){
-  this.tipo = tipo;
-}
-public void setDato(String dato){
-  this.dato = dato;
-}
-public String getDato(){
-  return dato;
-}
-public void setTipo(String tipo){
-  this.tipo=tipo;
-}
-public String getTipo(){
-  return tipo;
-}
+  public Device(String tipo, String id){
+    this.tipo = tipo;
+    this.id = id;
+  }
+  public String getId(){
+    return id;
+  }
+  public void setDato(String dato){
+    this.dato = dato;
+  }
+  public String getDato(){
+    return dato;
+  }
+  public void setTipo(String tipo){
+    this.tipo=tipo;
+  }
+  public String getTipo(){
+    return tipo;
+  }
 
 }
