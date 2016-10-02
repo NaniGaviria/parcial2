47b665104fd2dc0130a126f6dd1adc07ffb57ea6
Roberto Robles <robertodavidrobles@gmail.com>
2016-10-01 23:22:30 -0500

    Add constructor

--- Estrella.java
+++ Estrella.java
@@ -1,5 +1,13 @@
 public class Estrella extends Lan {
 
-  
+protected String pass = "";
+
+
+  public Estrella(String tipoDeComunicacion,String capacidad,String pass){
+    super(tipoDeComunicacion,capacidad);
+    this.pass = pass;
+  }
+
+
 
 }
