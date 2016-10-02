8c278c4180243fb8e1be0610ab3a167cc96f104f
Lina Oll <ferchalina88@hotmail.com>
2016-10-02 16:06:50 -0500

    se crea el constructor de la clase bus

---
+++ Bus.java
@@ -0,0 +1,11 @@
+public class Bus (){
+
+  protected String pass = "";
+
+
+    public Bus(tipoDeComunicacion,String capacidad,String pass){
+      super(tipoDeComunicacion,capacidad);
+      this.pass = pass;
+    }
+
+}
