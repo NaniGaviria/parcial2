c5721d74c10d149672bb79451eac71cd5420ed3f
Ana Gaviria <anagaviria19@gmail.com>
2016-10-02 20:38:02 -0500

    Se creo el metodo para crear la red bus y el metodo de la red bus

--- Vista.java
+++ Vista.java
@@ -18,7 +18,7 @@ public class Vista {
     switch (opcion) {
       case 1: crearEstrella();
       case 2: crearAnillo();
-    //   case 3:
+      case 3: crearBus();
     //   case 4:
      }
   }
@@ -42,6 +42,7 @@ public class Vista {
     pass = input.nextLine();
 
     redEstrella= new Estrella(tipoCom, capacidad, pass);
+    menuEstrella();
   }
 
   //Método para mostrar el menú de la estrela
@@ -106,6 +107,67 @@ public class Vista {
   }
 
   public void menuAnillo() {
+
+  }
+
+  public void crearBus (){
+    String tipoCom;
+    int capacidad;
+    String pass;
+
+    System.out.println("Digite el tipo de comunicación");
+    tipoCom = input.nextLine();
+
+    System.out.println("Digite la capacidad de la red");
+    capacidad = input.nextInt();
+
+    input.nextLine();
+
+    System.out.println ("Digita una contraseña para la red");
+    pass = input.nextLine();
+
+    redBus= new Bus(tipoCom, capacidad, pass);
+  }
+
+  public void menuBus (){
+    String t; //varables que almacena el tipo de dispositivo
+    int i; //Variable que almacena la posición del dispositivo
+    System.out.println ("Seleccione la acción a ejecutar \n"+"1. Agregar dispositivos a la red \n"+"2.Quitar dispositivo de la red" +
+                                                           "\n3.Enviar datos" + "\n4.Recibir datos" + "\n5.Cantidad de dispositivos en la red");
+    opcion = input.nextInt();
+    switch (opcion) {
+      case 1:
+      System.out.println("Digite tipo de dispositivo a agregar");
+      t = input.nextLine();
+      redBus.addDevice(t);
+      break;
+
+      case 2:
+      System.out.println ("Digite el id del dispositivo a quitar");
+      i = input.nextInt();
+      redBus.removeDevice(i);
+      break;
+
+      case 3:
+      System.out.println ("Dato a enviar");
+      t = input.nextLine();
+      System.out.println ("Posición del dispositivo en la red");
+      i = input.nextInt();
+      redBus.enviarDato(i,t);
+      break;
+
+      case 4:
+      System.out.println ("Ubicación del dispositivo");
+      i = input.nextInt();
+      redBus.removeDevice(i);
+      break;
+
+      case 5:
+      System.out.println ("Listando dispositivos");
+      redBus.printDevices();
+      break;
+
+     }
 
   }
 
