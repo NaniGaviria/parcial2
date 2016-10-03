fada60954257bc78fd7fb1ff1a9be8a5e19169c9
Ana Gaviria <anagaviria19@gmail.com>
2016-10-02 19:45:58 -0500

    Indentacion en la clase Vista

--- Vista.java
+++ Vista.java
@@ -1,42 +1,58 @@
-import java.util.Scanner;
 
+import java.util.Scanner;
 public class Vista {
 
   Scanner input = new java.util.Scanner(System.in);
   Estrella redEstrella;
   Anillo redAnillo;
   Bus redBus;
+  //Hub redHub;
+
   int opcion;
 
   public void iniciar () {
 
     System.out.println ("Seleccione el tipo de red a crear \n"+"1. Estrella \n"+"2.Anillo" +
-    "\n3.Bus" + "\n4.Hub");
+                                                           "\n3.Bus");
     opcion = input.nextInt();
     switch (opcion) {
       case 1: crearEstrella();
-      //   case 2:
-      //   case 3:
-      //   case 4:
-    }
+    //   case 2:
+    //   case 3:
+    //   case 4:
+     }
   }
 
+  //Metodo para crear estrella
   public void crearEstrella () {
 
     String tipoCom;
     int capacidad;
     String pass;
-    input.nextLine();
+
     System.out.println("Digite el tipo de comunicación");
     tipoCom = input.nextLine();
 
     System.out.println("Digite la capacidad de la red");
     capacidad = input.nextInt();
+
     input.nextLine();
+
     System.out.println ("Digita una contraseña para la red");
     pass = input.nextLine();
 
-    redEstrella = new Estrella(tipoCom, capacidad, pass);
+    redEstrella= new Estrella(tipoCom, capacidad, pass);
   }
+
+  //Método para mostrar el menú de la estrela
+  public void menuEstrella () {
+
+
+
+  }
+
+
+
+
 
 }
